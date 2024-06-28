15302276b60959e761d570459f2f4d74fb736784
gabrielperao <gabrielperao@hotmail.com>
2024-06-28 15:40:20 -0300

    setup mydos

---
+++ mydos/bios1.h
@@ -0,0 +1,24 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2024 Gabriel Barbosa de Amorim Perão <gabrielperao@usp.br>
+#    SPDX-FileCopyrightText: 2024 Ramon Moreira Machado <ramon1@usp.br>
+ *
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative work from SYSeg (https://gitlab.com/monaco/syseg)
+ *  and contains modifications carried out by the following author(s):
+ *  Gabriel Barbosa de Amorim Perão <gabrielperao@usp.br>
+ *  Ramon Moreira Machado <ramon1@usp.br>
+ */
+
+#ifndef BIOS1_H
+#define BIOS1_H
+
+void __attribute__((fastcall)) clear (void);
+void __attribute__((fastcall)) kwrite(const char*);
+void __attribute__((fastcall)) kwriteln(const char*);
+void __attribute__((fastcall)) fatal(const char*);
+void __attribute__((fastcall)) load_kernel(void);
+void __attribute__((fastcall)) set_cursor(char, char);
+
+#endif
---
+++ mydos/logo.c
@@ -0,0 +1,37 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2024 Gabriel Barbosa de Amorim Perão <gabrielperao@usp.br>
+ *    SPDX-FileCopyrightText: 2024 Ramon Moreira Machado <ramon1@usp.br>
+ *
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative work from SYSeg (https://gitlab.com/monaco/syseg)
+ *  and contains modifications carried out by the following author(s):
+ *  Gabriel Barbosa de Amorim Perão <gabrielperao@usp.br>
+ *  Ramon Moreira Machado <ramon1@usp.br>
+ */
+
+const char logo[] =                                                                         
+    "                                                                       "
+    "                                                                       "       
+    "        +@@@@@@@@@@@@-                                                 "       
+    "    =@@@@@@@@@@@@@@@@@@@=                                              "       
+    "        @@@@@@@@@@@@@@@@@@                           @@@@@@@           "       
+    "          @@@@@@@@@@@@@@@@@=                        @@@@@@@@@@@=       "       
+    "              @@@@@@@@@@@@@@@@                    @@@@@@@@@@@@@@@@     "       
+    "                 @@@@@@@@@@@@@@@      @@    @#  @@@@@@@@@@@@@@*        "       
+    "                   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@               "      
+    "                       @@@@@@@@@@@@@@@@@@@@@@@@@@@-                    "       
+    "                         @@@@@@@@@@@@@@@@@@@                           "       
+    "                            @@@@@@@@@@@@@@@                            "       
+    "                            @@@@@@@@@@@@@                              "       
+    "                           @@ @@@@@@@@                                 "       
+    "                               @@@   @                                 "       
+    "                                                                       "
+    "                                                                       "                                                                                
+    "                                                       .@@             "       
+    "                 @@@@  =@@@ @@  @@    @@.@@@  @@*=@@+ @@               "       
+    "                 +@ @  @ @@  @@@:     @    @ @@    @@ @@@@             "       
+    "                 +@ #@@@ @@   @@      @   #@ *@    @@    @@            "       
+    "                 @@  @@  @@   @@      @@@@@   @@@@@@ -@@@@             "       
+    "                                                                       ";
